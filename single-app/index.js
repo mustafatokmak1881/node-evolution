9dc66cc856d188fc95dd9539fd5c3be09933c957
mehmet.tokmak <mehmet.tokmak@beyn.com.tr>
2024-12-13 23:35:44 +0300

    .

--- single-app/index.js
+++ single-app/index.js
@@ -1,9 +1,22 @@
 const functions = require("../single-app/modules/functions");
 
-(async () => {
-  const fileList = await functions.getDir();
-  fileList.forEach(async (file, key) => {
-    const result = await functions.runApp(file);
-    console.log(result);
-  });
-})();
+let oldFileList = [];
+
+const app = () => {
+  console.log({ running: new Date() });
+  (async () => {
+    const fileList = await functions.getDir();
+    if (oldFileList != fileList && fileList.length > 0) {
+      fileList.forEach(async (file, key) => {
+        const result = await functions.runApp(file);
+        console.log(result);
+      });
+
+      oldFileList = fileList;
+    }
+  })();
+};
+
+setInterval(() => {
+  app();
+}, 500);
