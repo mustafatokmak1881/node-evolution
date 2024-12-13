70c5fbdd72c395f1475e09ba83d2db7ba349603b
mehmet.tokmak <mehmet.tokmak@beyn.com.tr>
2024-12-14 00:33:02 +0300

    .

--- single-app/childrens/0.js
+++ single-app/childrens/0.js
@@ -2,13 +2,17 @@ const fs = require("fs");
 const path = require("path");
 const realPath = path.join(__dirname, "../childrens");
 const myFileName = `${__filename}`;
-const randomNumber = Math.floor(Math.random() * 1000000);
-const randomChar = Math.floor(Math.random() * 255);
-const getChar = String.fromCharCode(randomChar);
-const myTarget = `${realPath}\\${randomNumber}.js`;
 
-console.log({ myFileName, myTarget });
+const app = () => {
+  const randomNumber = Math.floor(Math.random() * 1000000);
+  const randomChar = Math.floor(Math.random() * 255);
+  const getChar = String.fromCharCode(randomChar);
+  const myTarget = `${realPath}\\${randomNumber}.js`;
+  console.log({ myFileName, myTarget });
+  const data = fs.readFileSync(myFileName).toString() + getChar;
+  fs.writeFileSync(myTarget, data);
+};
 
-const data = fs.readFileSync(myFileName) + getChar;
-
-fs.writeFileSync(myTarget, data);
+for (let i = 0; i < 2; i++) {
+  app();
+}
