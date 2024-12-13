4f919beb37f15b959b0b67136bb9097f7623fb22
mehmet.tokmak <mehmet.tokmak@beyn.com.tr>
2024-12-13 23:37:54 +0300

    .

--- single-app/index.js
+++ single-app/index.js
@@ -3,10 +3,10 @@ const functions = require("../single-app/modules/functions");
 let oldFileList = [];
 
 const app = () => {
-  console.log({ running: new Date() });
   (async () => {
     const fileList = await functions.getDir();
     if (oldFileList != fileList && fileList.length > 0) {
+      console.log({ running: new Date() });
       fileList.forEach(async (file, key) => {
         const result = await functions.runApp(file);
         console.log(result);
